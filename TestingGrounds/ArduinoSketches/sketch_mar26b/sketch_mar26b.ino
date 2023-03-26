9762e9b709b27e3cb596be0b53267802d66d5d0f
SWSpratlin <120224974+SWSpratlin@users.noreply.github.com>
2023-03-26 16:33:38 -0400

    Added Libraries and Organized folders
    basic setup for sharing purposes

---
+++ TestingGrounds/ArduinoSketches/sketch_mar26b/sketch_mar26b.ino
@@ -0,0 +1,14 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
