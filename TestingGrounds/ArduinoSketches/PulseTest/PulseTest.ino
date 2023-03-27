3ece2e08cb3f6a0a1ba944d8e763decfd87b43b1
SWSpratlin <120224974+SWSpratlin@users.noreply.github.com>
2023-03-27 10:34:14 -0400

    Added FastLED lib
    Big push, need the additional Lib

--- TestingGrounds/ArduinoSketches/PulseTest/PulseTest.ino
+++ TestingGrounds/ArduinoSketches/PulseTest/PulseTest.ino
@@ -1,8 +1,4 @@
-#include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
-#endif
+#include <FastLED.h>
 
-// Pin and LED count for the one strip I have
-#define LED_PIN 10
-#define LED_COUNT 150
+#define NUM_LEDS 150
+#define DATA_PIN 10
